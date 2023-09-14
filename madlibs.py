d5d5d5496a75deb433b3cd1a6bc5f62847371e79
Hameed Agunbiade-Sulaimon <hagunbiadesulaimon@yahoo.com>
2023-09-14 17:49:35 +0100

    I wrote a madlibs program that words from user

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+""" Mad Libs python project"""
+' Mad Libs python project'
+
+# I am writing Mad libs project in python. It takes in strings and print them in sentences.
+
+
+noun = input("Enter a noun:")
+verb = input("Enter a verb:")
+noun1 = input( "Enter a noun1:")
+
+
+Madlibs = "I am taken one" + " " + noun + " " + "at a time." + "I am" + " " + verb + " " + " but I don't care" + " " + "I will get to my destination." +  " "+" I have had many untold" + " " + noun1 +" " + ". From my childhold to my adulthold but with Allah by side, " + " " + "I won't give up."
+print( Madlibs)
